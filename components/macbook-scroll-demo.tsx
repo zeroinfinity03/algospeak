1b7f350229a5e560c54f86f750959c5ac03c58f4
zeroinfinity03 <zeroinfinity03@gmail.com>
2025-07-25 01:08:04 -0700

    first commit

--- components/macbook-scroll-demo.tsx
+++ components/macbook-scroll-demo.tsx
@@ -3,7 +3,7 @@ import { MacbookScroll } from "@/components/ui/macbook-scroll";
 
 export default function MacbookScrollDemo() {
     return (
-        <div className="w-full overflow-hidden bg-white dark:bg-[#0B0B0F]">
+        <div className="w-full overflow-hidden bg-white dark:bg-[#0B0B0F] pb-20">
             <MacbookScroll
                 title={
                     <span>
@@ -11,8 +11,8 @@ export default function MacbookScrollDemo() {
                     </span>
                 }
                 badge={
-                    <a href="https://peerlist.io/manuarora">
-                        <Badge className="h-10 w-10 -rotate-12 transform" />
+                    <a href="https://github.com/zeroinfinity03" target="_blank" rel="noopener noreferrer">
+                        <GitHubBadge className="h-10 w-10 -rotate-12 transform" />
                     </a>
                 }
                 showGradient={false}
@@ -21,46 +21,24 @@ export default function MacbookScrollDemo() {
     );
 }
 
-// Peerlist logo
-const Badge = ({ className }: { className?: string }) => {
+// GitHub logo
+const GitHubBadge = ({ className }: { className?: string }) => {
     return (
-        <svg
-            width="24"
-            height="24"
-            viewBox="0 0 56 56"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className={className}
-        >
-            <path
-                d="M56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28Z"
-                fill="#00AA45"
-            ></path>
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M28 54C42.3594 54 54 42.3594 54 28C54 13.6406 42.3594 2 28 2C13.6406 2 2 13.6406 2 28C2 42.3594 13.6406 54 28 54ZM28 56C43.464 56 56 43.464 56 28C56 12.536 43.464 0 28 0C12.536 0 0 12.536 0 28C0 43.464 12.536 56 28 56Z"
-                fill="#219653"
-            ></path>
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M27.0769 12H15V46H24.3846V38.8889H27.0769C34.7305 38.8889 41 32.9048 41 25.4444C41 17.984 34.7305 12 27.0769 12ZM24.3846 29.7778V21.1111H27.0769C29.6194 21.1111 31.6154 23.0864 31.6154 25.4444C31.6154 27.8024 29.6194 29.7778 27.0769 29.7778H24.3846Z"
-                fill="#24292E"
-            ></path>
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M18 11H29.0769C36.2141 11 42 16.5716 42 23.4444C42 30.3173 36.2141 35.8889 29.0769 35.8889H25.3846V43H18V11ZM25.3846 28.7778H29.0769C32.1357 28.7778 34.6154 26.39 34.6154 23.4444C34.6154 20.4989 32.1357 18.1111 29.0769 18.1111H25.3846V28.7778Z"
+        <div className={`${className} bg-black rounded-full flex items-center justify-center`}>
+            <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
                 fill="white"
-            ></path>
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M17 10H29.0769C36.7305 10 43 15.984 43 23.4444C43 30.9048 36.7305 36.8889 29.0769 36.8889H26.3846V44H17V10ZM19 12V42H24.3846V34.8889H29.0769C35.6978 34.8889 41 29.7298 41 23.4444C41 17.1591 35.6978 12 29.0769 12H19ZM24.3846 17.1111H29.0769C32.6521 17.1111 35.6154 19.9114 35.6154 23.4444C35.6154 26.9775 32.6521 29.7778 29.0769 29.7778H24.3846V17.1111ZM26.3846 19.1111V27.7778H29.0769C31.6194 27.7778 33.6154 25.8024 33.6154 23.4444C33.6154 21.0864 31.6194 19.1111 29.0769 19.1111H26.3846Z"
-                fill="#24292E"
-            ></path>
-        </svg>
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"
+                />
+            </svg>
+        </div>
     );
 };
 
